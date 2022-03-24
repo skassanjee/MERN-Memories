1e9c3d9e1d8a10eac35d2d90584f4df0696f16f1
skassanjee <skassanjee@gmail.com>
2022-03-24 01:25:58 -0600

    part 1

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,23 +1,42 @@
-import React from 'react'
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
-import memories from './Assets/Images/memories.png'
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import memories from "./Assets/Images/memories.png";
+import Form from "./Components/Form/Form";
+import Posts from "./Components/Posts/Posts";
+import useStyles from './styles'
 
 const App = () => {
+
+    const classes = useStyles()
+
   return (
     <Container maxWidth="lg">
-        <AppBar position="static" color="inherit">
-            <Typography variant="h2" align='center'>Memoriezzz</Typography>
-            <img src={memories} alt="memories" height="60" />
-        </AppBar>
-        <Grow in>
-            <Container>
-                <Grid container justifyContent='space-between' alignItems='stretch' spacing={4}>
-                    <Grid item
-                </Grid>
-            </Container>
-        </Grow>
-    </Container>
-  )
-}
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memoriezzz
+        </Typography>
+        <img className={classes.image} src={memories} alt="memories" height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={4}
+          >
+            <Grid item xs={12} sm={7}>
+                <Posts />
+            </Grid>
 
-export default App
+            <Grid item xs={12} sm={4}>
+                <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
+
+export default App;
---
+++ Client/src/Components/Posts/Posts.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Post from './Post/Post'
+import useStyles from './styles'
+
+const Posts = () => {
+  return (
+    <div>
+        <Post />
+    </div>
+  )
+}
+
+export default Posts
